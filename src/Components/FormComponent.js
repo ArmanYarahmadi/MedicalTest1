797fa8ea646e18c8a784112de54744d494ddd498
ArmanYarahmadi <arm1377arman@gmail.com>
2020-06-04 23:04:21 +0430

    form part2

--- src/Components/FormComponent.js
+++ src/Components/FormComponent.js
@@ -1,61 +1,58 @@
-import React from "react";
-import { Form, FormGroup, Input, Label, Button, FormText } from "reactstrap";
+import React, { useState } from "react";
+import { Form, FormGroup, Input, Label, Button, ButtonGroup } from "reactstrap";
 
 const FormComponent = (props) => {
+  const [selected, setSelected] = useState(1);
+
   return (
-    <div className="formContainer">
+    <div className="container">
       <Form className="formComponent">
         <h5>تخصص مورد نیاز را انتخاب کنید</h5>
-        <div className="selectorContainer">
-          <FormGroup className="itemSelect">
-            <Input
-              type="select"
-              name="select"
-              id="exampleSelect"
-              className="selector"
-            >
-              <option className="option">پزشک عمومی</option>
-              <option className="option">پزشک خصوصی</option>
+        <div>
+          <FormGroup className="selector">
+            <Input type="select" name="select" id="exampleSelect">
+              <option>پزشک عمومی</option>
+              <option>پزشک خصوصی</option>
             </Input>
           </FormGroup>
         </div>
         <h5>جنسیت پزشک مورد نظر را انتخاب کنید</h5>
-        <div className="radioboxContainer">
-          <FormGroup check inline className="">
-            <div className="radiocheck">
-              <FormGroup check>
-                <Label check>
-                  <legend>
-                    <Button type="radio" name="radio1">
-                      همه
-                    </Button>
-                  </legend>
-                </Label>
-              </FormGroup>
+        <div className="">
+          <ButtonGroup checkbox className="radiobox col-12">
+            <div className="buttons col-4">
+              <Button
+                outline
+                color="info"
+                size="lg"
+                onClick={() => setSelected(1)}
+                active={selected === 1}
+              >
+                <legend>همه</legend>
+              </Button>
             </div>
-            <div className="radiocheck">
-              <FormGroup check>
-                <Label check>
-                  <legend>
-                    <Button type="radio" name="radio1">
-                      زن
-                    </Button>
-                  </legend>
-                </Label>
-              </FormGroup>
+            <div className="buttons col-4">
+              <Button
+                outline
+                color="info"
+                size="lg"
+                onClick={() => setSelected(2)}
+                active={selected === 2}
+              >
+                <legend>زن</legend>
+              </Button>
             </div>
-            <div className="radiocheck">
-              <FormGroup check disabled>
-                <Label check>
-                  <legend>
-                    <Button type="radio" name="radio1">
-                      مرد
-                    </Button>
-                  </legend>
-                </Label>
-              </FormGroup>
+            <div className="buttons col-4">
+              <Button
+                outline
+                color="info"
+                size="lg"
+                onClick={() => setSelected(3)}
+                active={selected === 3}
+              >
+                <legend>مرد</legend>
+              </Button>
             </div>
-          </FormGroup>
+          </ButtonGroup>
         </div>
       </Form>
     </div>
