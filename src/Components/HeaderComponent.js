55734753411a9280038cc28dc71757eb587b7c60
ArmanYarahmadi <arm1377arman@gmail.com>
2020-06-06 17:05:08 +0430

    final

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
-import {
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavbarToggler,
-  Collapse,
-  NavItem,
-} from "reactstrap";
+import { Navbar, Nav, NavbarToggler, Collapse, NavItem } from "reactstrap";
 
 const Header = (props) => {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -17,45 +10,44 @@ const Header = (props) => {
 
   return (
     <div className="header row fixed-top">
-      <div className="mr-auto">
-        <div className="footer-item col-4">
-          <a className="footer-item-text nav-link" href="/">
-            <span className="fa fa-search fa-2x"></span>
-          </a>
-        </div>
-      </div>
       <div className="navBarContainer">
         <Navbar light expand="md">
           <div className="container">
             <NavbarToggler className="ml-auto" onClick={toggleNav} />
-            <NavbarBrand href="/"></NavbarBrand>
             <Collapse isOpen={isNavOpen} navbar>
               <Nav navbar>
                 <NavItem className="mr-4">
                   <a className="nav-link active" href="/">
-                    خانه <span className="fa fa-home fa-lg"></span>
+                    <span className="fa fa-home fa-lg"></span> خانه
                   </a>
                 </NavItem>
                 <NavItem className="mr-4">
                   <a className="nav-link" href="/">
-                    درباره ما <span className="fa fa-info fa-lg"></span>
+                    <span className="fa fa-info fa-lg"></span> درباره ما
                   </a>
                 </NavItem>
                 <NavItem className="mr-4">
                   <a className="nav-link" href="/">
-                    فهرست <span className="fa fa-list fa-lg"></span>
+                    <span className="fa fa-list fa-lg"></span> فهرست
                   </a>
                 </NavItem>
                 <NavItem className="mr-4">
                   <a className="nav-link" href="/">
-                    تماس با ما{" "}
-                    <span className="fa fa-address-card fa-lg"></span>
+                    <span className="fa fa-address-card fa-lg"></span> تماس با
+                    ما
                   </a>
                 </NavItem>
               </Nav>
             </Collapse>
           </div>
         </Navbar>
+      </div>
+      <div className="mr-auto">
+        <div className="footer-item col-4">
+          <a className="footer-item-text nav-link" href="/">
+            <span className="fa fa-search fa-2x"></span>
+          </a>
+        </div>
       </div>
     </div>
   );
