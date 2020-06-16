71556c608415e0041ccddd9840e55464204fe431
Arman Yarahmadi <arm1377arman@gmail.com>
2020-06-16 09:35:26 +0430

    changes

--- src/Components/DoctorComponents/DoctorsLoginComponent/DoctorsLoginComponent.js
+++ src/Components/DoctorComponents/DoctorsLoginComponent/DoctorsLoginComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import {
   Card,
   CardBody,
@@ -12,120 +12,135 @@ import { Control, Form, Errors } from "react-redux-form";
 import Header from "../../Headers/HomeHeader/HeaderComponent";
 import "./styles.css";
 
-function DoctorsLogin(props) {
-  const [show, setShow] = useState(false);
-  const [alertDescription, setAlertDescription] = useState("");
-  const [alertColor, setAlertColor] = useState("");
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+const isNumber = (val) => !isNaN(Number(val));
 
-  const required = (val) => val && val.length;
-  const maxLength = (len) => (val) => !val || val.length <= len;
-  const minLength = (len) => (val) => val && val.length >= len;
-  const isNumber = (val) => !isNaN(Number(val));
+class DoctorsLogin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      alertDescription: "",
+      alertColor: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  const handleSubmit = (values) => {
-    props
+  handleSubmit = (values) => {
+    this.props
       .postDoctorsLogin(values.phoneNumber, values.password)
       .then((res) => {
         console.log(res);
-        setShow(true);
-        setAlertColor("success");
-        setAlertDescription("خوش آمدید");
+        this.setState({
+          show: true,
+          alertColor: "success",
+          alertDescription: "خوش آمدید",
+        });
         localStorage.setItem("Token", res.data.Token);
       })
       .catch((err) => {
-        setAlertDescription(err.response.data.message);
-        setShow(true);
-        setAlertColor("danger");
+        this.setState({
+          show: true,
+          alertColor: "danger",
+          alertDescription: err.response.data.message,
+        });
+
         console.log(err);
       });
-    props.resetDoctorsLoginForm();
+    this.props.resetDoctorsLoginForm();
   };
 
-  return (
-    <>
-      <Header headerTitle="ورود" />
-      <div className="container col-12 col-sm-6">
-        <Card className="loginCard">
-          <img
-            src={require("../../../assets/images/user.png")}
-            alt="user"
-            className="userImg"
-          />
-          <CardBody className="login-body">
-            <Form
-              model="DoctorsLogin"
-              onSubmit={(values) => handleSubmit(values)}
-            >
-              <Col className="col-12 col-sm-8 phoneContainer" dir="ltr">
-                <Collapse isOpen={show}>
-                  <Alert color={alertColor}>{alertDescription}</Alert>
-                </Collapse>
-                <Label htmlFor="phoneNumber">
-                  لطفا شماره همراه خود را وارد کنید
-                </Label>
-                <Control.text
-                  model=".phoneNumber"
-                  id="phoneNumber"
-                  name="phoneNumber"
-                  placeholder="شماره همراه"
-                  className="form-control text-center mt-2 phone-number"
-                  validators={{
-                    required,
-                    isNumber,
-                    minLength: minLength(11),
-                    maxLength: maxLength(11),
-                  }}
-                  persist
-                />
-                <Errors
-                  className="text-danger"
-                  model=".phoneNumber"
-                  show="touched"
-                  messages={{
-                    required: "اجباری ",
-                    isNumber: "فقط اعداد ",
-                    minLength: "11 رقم ",
-                    maxLength: "11 رقم ",
-                  }}
-                />
-              </Col>
-              <Col className="col-12 col-sm-8 passwordContainer" dir="ltr">
-                <Label htmlFor="password">لطفا رمز خود را وارد نمایید</Label>
-                <Control
-                  type="password"
-                  model=".password"
-                  id="password "
-                  name="password"
-                  placeholder="رمز عبور"
-                  className="form-control text-center mt-2 password"
-                  validators={{
-                    required,
-                    isNumber,
-                    minLength: minLength(8),
-                    maxLength: maxLength(256),
-                  }}
-                />
-                <Errors
-                  className="text-danger"
-                  model=".password"
-                  show="touched"
-                  messages={{
-                    required: "اجباری ",
-                    isNumber: "فقط اعداد ",
-                    minLength: "حداقل 8 رقم ",
-                    maxLength: "حداکثر 256 رقم",
-                  }}
-                />
-              </Col>
-              <Button type="submit" color="primary" className="login-btn">
-                ورود
-              </Button>
-            </Form>
-          </CardBody>
-        </Card>
-      </div>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <Header headerTitle="ورود" />
+        <div className="container col-12 col-sm-6">
+          <Card className="loginCard">
+            <img
+              src={require("../../../assets/images/user.png")}
+              alt="user"
+              className="userImg"
+            />
+            <CardBody className="login-body">
+              <Form
+                model="DoctorsLogin"
+                onSubmit={(values) => this.handleSubmit(values)}
+              >
+                <Col className="col-12 col-sm-8 phoneContainer" dir="ltr">
+                  <Collapse isOpen={this.state.show}>
+                    <Alert color={this.state.alertColor}>
+                      {this.state.alertDescription}
+                    </Alert>
+                  </Collapse>
+                  <Label htmlFor="phoneNumber">
+                    لطفا شماره همراه خود را وارد کنید
+                  </Label>
+                  <Control.text
+                    model=".phoneNumber"
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    placeholder="شماره همراه"
+                    className="form-control text-center mt-2 phone-number"
+                    validators={{
+                      required,
+                      isNumber,
+                      minLength: minLength(11),
+                      maxLength: maxLength(11),
+                    }}
+                    persist
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".phoneNumber"
+                    show="touched"
+                    messages={{
+                      required: "اجباری ",
+                      isNumber: "فقط اعداد ",
+                      minLength: "11 رقم ",
+                      maxLength: "11 رقم ",
+                    }}
+                  />
+                </Col>
+                <Col className="col-12 col-sm-8 passwordContainer" dir="ltr">
+                  <Label htmlFor="password">لطفا رمز خود را وارد نمایید</Label>
+                  <Control
+                    type="password"
+                    model=".password"
+                    id="password "
+                    name="password"
+                    placeholder="رمز عبور"
+                    className="form-control text-center mt-2 password"
+                    validators={{
+                      required,
+                      isNumber,
+                      minLength: minLength(8),
+                      maxLength: maxLength(256),
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".password"
+                    show="touched"
+                    messages={{
+                      required: "اجباری ",
+                      isNumber: "فقط اعداد ",
+                      minLength: "حداقل 8 رقم ",
+                      maxLength: "حداکثر 256 رقم",
+                    }}
+                  />
+                </Col>
+                <Button type="submit" color="primary" className="login-btn">
+                  ورود
+                </Button>
+              </Form>
+            </CardBody>
+          </Card>
+        </div>
+      </>
+    );
+  }
 }
 
 export default DoctorsLogin;
--- src/Components/PatientComponents/PatientsLoginComponent/PatientsLoginComponent.js
+++ src/Components/PatientComponents/PatientsLoginComponent/PatientsLoginComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import {
   Card,
   CardBody,
@@ -12,161 +12,187 @@ import { Control, Form, Errors } from "react-redux-form";
 import Header from "../../Headers/HomeHeader/HeaderComponent";
 import "./styles.css";
 
-function PatientsLogin(props) {
-  const [isOpen, setIsOpen] = useState(false);
-  const [tempToken, setTempToken] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [show, setShow] = useState(false);
-  const [alertDescription, setAlertDescription] = useState("");
-  const [alertColor, setAlertColor] = useState("");
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+const isNumber = (val) => !isNaN(Number(val));
 
-  const required = (val) => val && val.length;
-  const maxLength = (len) => (val) => !val || val.length <= len;
-  const minLength = (len) => (val) => val && val.length >= len;
-  const isNumber = (val) => !isNaN(Number(val));
+class PatientsLogin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      show: false,
+      tempToken: "",
+      phoneNumber: "",
+      alertDescription: "",
+      alertColor: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmitPassword = this.handleSubmitPassword.bind(this);
+  }
 
-  const handleSubmit = (values) => {
-    setPhoneNumber(values.phoneNumber);
-    props
+  handleSubmit = (values) => {
+    this.setState({
+      phoneNumber: values.phoneNumber,
+    });
+    this.props
       .postPatientsLogin(values.phoneNumber)
       .then((res) => {
-        setAlertDescription("تایید تلفن همراه");
-        setShow(true);
-        setAlertColor("success");
-        setTempToken(res.data.token);
-        setIsOpen(true);
+        this.setState({
+          isOpen: true,
+          show: true,
+          alertDescription: "تایید تلفن همراه",
+          alertColor: "success",
+          tempToken: res.data.token,
+        });
       })
       .catch((err) => {
-        setAlertDescription(
-          "شماره همراه وارد شده معتبر نمی باشد لطفا مجددا تلاش کنید"
-        );
-        setShow(true);
-        setAlertColor("danger");
-        console.log(err);
-        props.resetPatientsLoginForm();
+        this.setState({
+          show: true,
+          alertDescription:
+            "شماره همراه وارد شده معتبر نمی باشد لطفا مجددا تلاش کنید",
+          alertColor: "danger",
+        });
+
+        this.props.resetPatientsLoginForm();
       });
   };
 
-  const handleSubmitPassword = (values) => {
-    props
-      .postPatientsLoginPassword(phoneNumber, tempToken, values.password)
+  handleSubmitPassword = (values) => {
+    this.props
+      .postPatientsLoginPassword(
+        this.state.phoneNumber,
+        this.state.tempToken,
+        values.password
+      )
       .then((res) => {
         localStorage.setItem("authToken", res.data.authToken);
-        setAlertDescription("شما با موفقیت اضافه شدید");
-        setShow(true);
-        setAlertColor("success");
-        setIsOpen(false);
-        props.resetPatientsLoginPasswordForm();
-        props.resetPatientsLoginForm();
+        console.log(res.data);
+        this.setState({
+          isOpen: false,
+          show: true,
+          alertDescription: "شما با موفقیت اضافه شدید",
+          alertColor: "success",
+        });
+
+        this.props.resetPatientsLoginPasswordForm();
+        this.props.resetPatientsLoginForm();
       })
       .catch((err) => {
-        setAlertDescription(" کد وارد شده معتبر نمی باشد لطفا مجددا تلاش کنید");
-        setShow(true);
-        setAlertColor("danger");
+        this.setState({
+          show: true,
+          alertDescription: " کد وارد شده معتبر نمی باشد لطفا مجددا تلاش کنید",
+          alertColor: "danger",
+        });
+
         console.log(err);
-        setIsOpen(true);
-        props.resetPatientsLoginPasswordForm();
+        this.props.resetPatientsLoginPasswordForm();
       });
   };
 
-  return (
-    <>
-      <Header headerTitle="ورود" />
-      <div className="container col-12 col-sm-6">
-        <Card className="loginCard">
-          <img
-            src={require("../../../assets/images/user.png")}
-            alt="user"
-            className="userImg"
-          />
-          <CardBody className="login-body">
-            <Form
-              model="PatientsLogin"
-              onSubmit={(values) => handleSubmit(values)}
-            >
-              <Col className="col-12 col-sm-8 phoneContainer" dir="ltr">
-                <Collapse isOpen={show}>
-                  <Alert color={alertColor}>{alertDescription}</Alert>
-                </Collapse>
-                <Label htmlFor="phoneNumber">
-                  لطفا شماره همراه خود را وارد کنید
-                </Label>
-                <Control.text
-                  model=".phoneNumber"
-                  id="phoneNumber"
-                  name="phoneNumber"
-                  placeholder="شماره همراه"
-                  className="form-control text-center mt-2 phone-number"
-                  validators={{
-                    required,
-                    isNumber,
-                    minLength: minLength(11),
-                    maxLength: maxLength(11),
-                  }}
-                  persist
-                />
-                <Errors
-                  className="text-danger"
-                  model=".phoneNumber"
-                  show="touched"
-                  messages={{
-                    required: "اجباری ",
-                    isNumber: "فقط اعداد ",
-                    minLength: "11 رقم ",
-                    maxLength: "11 رقم ",
-                  }}
-                />
-              </Col>
-              <Button type="submit" color="primary" className="login-btn">
-                تایید
-              </Button>
-            </Form>
-            <Collapse isOpen={isOpen}>
+  render() {
+    return (
+      <>
+        <Header headerTitle="ورود" />
+        <div className="container col-12 col-sm-6">
+          <Card className="loginCard">
+            <img
+              src={require("../../../assets/images/user.png")}
+              alt="user"
+              className="userImg"
+            />
+            <CardBody className="login-body">
               <Form
-                className="mt-3"
-                model="PatientsLoginPassword"
-                onSubmit={(values) => handleSubmitPassword(values)}
+                model="PatientsLogin"
+                onSubmit={(values) => this.handleSubmit(values)}
               >
-                <Label htmlFor="password">
-                  لطفا رمز ارسال شده را وارد کنید
-                </Label>
-                <Col className="col-12 col-sm-8 passwordContainer" dir="ltr">
-                  <Control
-                    type="password"
-                    model=".password"
-                    id="password"
-                    name="password"
-                    placeholder="رمز عبور"
-                    className="form-control text-center mt-2 password"
+                <Col className="col-12 col-sm-8 phoneContainer" dir="ltr">
+                  <Collapse isOpen={this.state.show}>
+                    <Alert color={this.state.alertColor}>
+                      {this.state.alertDescription}
+                    </Alert>
+                  </Collapse>
+                  <Label htmlFor="phoneNumber">
+                    لطفا شماره همراه خود را وارد کنید
+                  </Label>
+                  <Control.text
+                    model=".phoneNumber"
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    placeholder="شماره همراه"
+                    className="form-control text-center mt-2 phone-number"
                     validators={{
                       required,
                       isNumber,
-                      minLength: minLength(4),
-                      maxLength: maxLength(5),
+                      minLength: minLength(11),
+                      maxLength: maxLength(11),
                     }}
+                    persist
                   />
                   <Errors
                     className="text-danger"
-                    model=".password"
+                    model=".phoneNumber"
                     show="touched"
                     messages={{
                       required: "اجباری ",
                       isNumber: "فقط اعداد ",
-                      minLength: "حداقل 4 رقم ",
-                      maxLength: "حداکثر 5 رقم ",
+                      minLength: "11 رقم ",
+                      maxLength: "11 رقم ",
                     }}
                   />
                 </Col>
                 <Button type="submit" color="primary" className="login-btn">
-                  ورود
+                  تایید
                 </Button>
               </Form>
-            </Collapse>
-          </CardBody>
-        </Card>
-      </div>
-    </>
-  );
+              <Collapse isOpen={this.state.isOpen}>
+                <Form
+                  className="mt-3"
+                  model="PatientsLoginPassword"
+                  onSubmit={(values) => this.handleSubmitPassword(values)}
+                >
+                  <Label htmlFor="password">
+                    لطفا رمز ارسال شده را وارد کنید
+                  </Label>
+                  <Col className="col-12 col-sm-8 passwordContainer" dir="ltr">
+                    <Control
+                      type="password"
+                      model=".password"
+                      id="password"
+                      name="password"
+                      placeholder="رمز عبور"
+                      className="form-control text-center mt-2 password"
+                      validators={{
+                        required,
+                        isNumber,
+                        minLength: minLength(4),
+                        maxLength: maxLength(5),
+                      }}
+                    />
+                    <Errors
+                      className="text-danger"
+                      model=".password"
+                      show="touched"
+                      messages={{
+                        required: "اجباری ",
+                        isNumber: "فقط اعداد ",
+                        minLength: "حداقل 4 رقم ",
+                        maxLength: "حداکثر 5 رقم ",
+                      }}
+                    />
+                  </Col>
+                  <Button type="submit" color="primary" className="login-btn">
+                    ورود
+                  </Button>
+                </Form>
+              </Collapse>
+            </CardBody>
+          </Card>
+        </div>
+      </>
+    );
+  }
 }
 
 export default PatientsLogin;
